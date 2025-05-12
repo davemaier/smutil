c92de89b398eef4b8a0e680aa2448b3a31954d75
davemaier <david.maier@atempo.at>
2025-05-12 10:48:10 +0200

    feat: add log

--- packages/client-react/src/useTranslate.tsx
+++ packages/client-react/src/useTranslate.tsx
@@ -40,6 +40,10 @@ export function useTranslate(initialLang?: string, config?: ClientConfig) {
     loadingRef.current = loading;
   }, [loading]);
 
+  useEffect(() => {
+    console.log("smutil react client loaded");
+  }, [loading]);
+
   // Language change handler
   useEffect(() => {
     contextRef.current = {
